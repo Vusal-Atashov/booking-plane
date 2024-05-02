fb1135e80114dd17a00c72deacb4b30ba31a6d38
Vusal Atashov <156101309+Vusal-Atashov@users.noreply.github.com>
2024-05-02 15:21:40 +0400

    booking entity

--- src/main/java/dao/BookingEntity.java
+++ src/main/java/dao/BookingEntity.java
@@ -1,12 +1,55 @@
 package dao;
 
-import com.fasterxml.jackson.databind.cfg.MapperBuilder;
-
 import java.util.List;
+import java.util.Objects;
 
 public class BookingEntity {
-    private String id;
+    private static long MAX_ID=0;
+    private final long id;
     private String flightId;
     private List<String> passengerNames;
 
+    public BookingEntity(String flightId, List<String> passengerNames) {
+        this.id = ++MAX_ID;
+        this.flightId = flightId;
+        this.passengerNames = passengerNames;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(String flightId) {
+        this.flightId = flightId;
+    }
+
+    public List<String> getPassengerNames() {
+        return passengerNames;
+    }
+
+    public void setPassengerNames(List<String> passengerNames) {
+        this.passengerNames = passengerNames;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookingEntity that = (BookingEntity) o;
+        return id == that.id && Objects.equals(flightId, that.flightId) && Objects.equals(passengerNames, that.passengerNames);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, flightId, passengerNames);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{id=%d, flightId='%s', passengerNames=%s}", id, flightId, passengerNames);
+    }
 }
