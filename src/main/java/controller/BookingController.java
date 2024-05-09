db05d93df8f19e753e0c47edb27af5d479d5c4a0
Vusal Atashov <156101309+Vusal-Atashov@users.noreply.github.com>
2024-05-09 14:28:38 +0400

    Booking Service

--- src/main/java/controller/BookingController.java
+++ src/main/java/controller/BookingController.java
@@ -1,9 +1,17 @@
 package controller;
 
-import java.util.Scanner;
+import dto.BookingDTO;
 
-public class BookingController {
+import java.util.Collection;
+import java.util.List;
 
 
-
+public interface BookingController {
+    void saveBooking(Collection<BookingDTO> bookingDTOS);
+    Collection<BookingDTO> getAllBooking();
+    BookingDTO getOneBookings(int id);
+    List<BookingDTO> getAllBookingsByPassenger(List<String> passengerNames);
+    List<BookingDTO> getAllBookingsByPassenger(int flightId);
+    void removeBooking(int id);
 }
+
---
+++ src/main/java/controller/BookingControllerImpl.java
@@ -0,0 +1,48 @@
+package controller;
+
+import dao.BookingEntity;
+import dto.BookingDTO;
+import services.BookingService;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class BookingControllerImpl implements BookingController {
+    private final BookingService bookingService;
+
+    public BookingControllerImpl(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+
+    @Override
+    public void saveBooking(Collection<BookingDTO> bookingDTOS) {
+        bookingService.saveBookings(bookingDTOS);
+    }
+
+    @Override
+    public Collection<BookingDTO> getAllBooking() {
+        return bookingService.getBookingsAll();
+    }
+
+    @Override
+    public BookingDTO getOneBookings(int id) {
+        return bookingService.getOneBookingsBy(booking -> booking.getId() == (id));
+    }
+
+    @Override
+    public List<BookingDTO> getAllBookingsByPassenger(List<String> passengerNames) {
+        return bookingService.getAllBookingsByPassenger(booking -> booking.getPassengerNames().equals(passengerNames));
+    }
+
+    @Override
+    public List<BookingDTO> getAllBookingsByPassenger(int flightId) {
+        return bookingService.getAllBookingsByPassenger(booking -> booking.getFlightId() == (flightId));
+    }
+
+    @Override
+    public void removeBooking(int id) {
+        bookingService.cancelBooking(id);
+    }
+}
